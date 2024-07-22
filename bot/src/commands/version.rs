c292b5495438d9d07e73ade2e3493aaa4839357b
RasAllhague <50292283+RasAllhague@users.noreply.github.com>
2024-07-22 11:15:19 +0200

    implemented bot basics

---
+++ bot/src/commands/version.rs
@@ -0,0 +1,15 @@
+use crate::Data;
+
+type PoiseError = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, PoiseError>;
+
+static VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// Displays the current version of the bot running
+#[poise::command(slash_command)]
+pub async fn version(ctx: Context<'_>) -> Result<(), PoiseError> {
+    let response = format!("Current running version: {VERSION}");
+    ctx.say(response).await?;
+
+    Ok(())
+}
--- bot/src/lib.rs
+++ bot/src/lib.rs
@@ -1,3 +1,46 @@
+pub mod commands;
+
+use commands::version::version;
+use poise::serenity_prelude::{self as serenity};
+
+type PoiseError = Box<dyn std::error::Error + Send + Sync>;
+
+pub struct Data {}
+
+#[tokio::main]
+async fn run() -> Result<(), PoiseError> {
+    tracing_subscriber::fmt::init();
+
+    dotenvy::dotenv().ok();
+    let token = std::env::var("GEMUKI_TOKEN").expect("Missing GEMUKI_TOKEN.");
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::GUILD_MEMBERS;
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![version()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
+
+    Ok(())
+}
+
 pub fn main() {
-    
+    let result = run();
+
+    if let Some(err) = result.err() {
+        println!("Error: {err}");
+    }
 }
